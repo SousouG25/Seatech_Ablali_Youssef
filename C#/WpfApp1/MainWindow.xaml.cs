e078d8dbd38882844ebe6880eaee5dfec312d2b3
TABLE2 <78029790+Kergourlay-T@users.noreply.github.com>
2022-12-13 17:26:51 +0100

    c#1

---
+++ C#/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using ExtendedSerialPort;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ReliableSerialPort serialPort1;
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            serialPort1 = new ReliableSerialPort("COM9", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            serialPort1.Open();
+        }
+
+        private void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            //textBoxReception.Text += textBoxEmission.Text;
+            //textBoxEmission.Text = "\n";
+
+            serialPort1.WriteLine(textBoxEmission.Text);
+            textBoxEmission.Text = "\n";
+        }
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                buttonEnvoyer_Click(sender,e);
+
+            }
+
+
+        }
+       
+
+
+
+    }
+}
